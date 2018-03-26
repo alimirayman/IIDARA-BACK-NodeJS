1e9473bba8e823bf89ceb95fe37f4636758dc848
Mir Ayman Ali <alimirayman@outlook.com>
2018-03-27 05:43:34 +0600

    upgrade cards endpoint

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -1,17 +1,42 @@
 var express = require('express'),
   router = express.Router(),
-  cards = require('../models/cards'),
+  Cards = require('../models/cards'),
+  cardsValidator = require('../middlewares/cards')
   auth = require('../middlewares/auth')
 
 router.route('/card')
-  .post(function (req, res) {
-    
+  .post(auth.ifAdmin, cardsValidator.init, function (req, res) {
+    // create a new instance of the Cards
+    var card = new Cards(req.card)
+
+    // save a Card and check for errors
+    card.save(function (err) {
+      if (err)
+        return res.send(err)
+
+      res.status(201)
+      res.json({ message: 'card added!' })
+    })
   })
   .get(function (req, res) {
-    cards.find({
-      status: req.query.status
-    }, function (err, data) {
-      res.json(data)
+    let query = {}
+    if ( req.query.status ) query.status = req.query.status
+    if ( req.query.karigor_id ) query.karigor_id = req.query.karigor_id
+    Cards.find(query, function (err, data) {
+      res.json(data.map(el => {
+        return {
+          id: el._id,
+          karigor_id: el.karigor_id,
+          title: el.title,
+          description: el.description,
+          images: el.images,
+          attachments: el.attachments,
+          issue_data: el.issue_data,
+          occupy_date: el.occupy_date,
+          allocated_time: el.allocated_time,
+          comments: el.comments
+        }
+      }))
     })
     // res.json(req.query.status)
   })
